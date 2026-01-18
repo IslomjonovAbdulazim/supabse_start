75ec4e4e345e454ba25f0cff97c8ea04f4ecea62
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2026-01-18 22:16:34 +0500

    aa

--- lib/realtime/chat_page.dart
+++ lib/realtime/chat_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
@@ -8,8 +9,37 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  final supabase = Supabase.instance.client;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: StreamBuilder(
+            stream: supabase.from("messages").stream(primaryKey: ['id']),
+            builder: (context, asyncSnapshot) {
+              if (asyncSnapshot.hasData == false) {
+                return Center(child: CircularProgressIndicator());
+              }
+
+              final messages = asyncSnapshot.data!;
+              print(asyncSnapshot.data);
+              return ListView.builder(
+                itemCount: messages.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(messages[index]['text']),
+                    subtitle: Text(messages[index]['created_at'].toString()),
+                    leading: Text(messages[index]['id'].toString()),
+                  );
+                },
+              );
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
