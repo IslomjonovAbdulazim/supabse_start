fcbb06422411b24515e417fdc08b2558d40f4b1f
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2026-01-18 22:46:27 +0500

    aa 2

--- lib/realtime/chat_page.dart
+++ lib/realtime/chat_page.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 class ChatPage extends StatefulWidget {
@@ -25,20 +27,80 @@ class _ChatPageState extends State<ChatPage> {
               }
 
               final messages = asyncSnapshot.data!;
-              print(asyncSnapshot.data);
+
               return ListView.builder(
                 itemCount: messages.length,
                 itemBuilder: (context, index) {
                   return ListTile(
+                    onTap: () {
+                      final controller = TextEditingController(
+                        text: messages[index]['text'],
+                      );
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text("Edit"),
+                            content: TextField(controller: controller),
+                            actions: [
+                              CupertinoButton(
+                                onPressed: () async {
+                                  Get.back();
+                                  await supabase
+                                      .from("messages")
+                                      .update({"text": controller.text})
+                                      .eq('id', messages[index]['id']);
+                                },
+                                child: Text("Save"),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    },
                     title: Text(messages[index]['text']),
                     subtitle: Text(messages[index]['created_at'].toString()),
                     leading: Text(messages[index]['id'].toString()),
+                    trailing: CupertinoButton(
+                      onPressed: () async {
+                        await supabase
+                            .from("messages")
+                            .delete()
+                            .eq('id', messages[index]['id']);
+                      },
+                      child: Icon(Icons.delete),
+                    ),
                   );
                 },
               );
             },
           ),
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          final controller = TextEditingController();
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: Text("Create"),
+                content: TextField(controller: controller),
+                actions: [
+                  CupertinoButton(
+                    onPressed: () async {
+                      Get.back();
+                      await supabase.from("messages").insert({
+                        "text": controller.text,
+                      });
+                    },
+                    child: Text("Create"),
+                  ),
+                ],
+              );
+            },
+          );
+        },
       ),
     );
   }
